ada31ac62f493b7602365d5336ef9c2d7ea9dc1a
dipanjan97 <dipanjandey@hotmail.com>
2020-11-09 14:08:14 +0530

    Version 2 as on Nov 2020
    +New activity
    +Set of Questions

--- app/src/main/java/com/example/quizzz/QuizQuestionsActivity.kt
+++ app/src/main/java/com/example/quizzz/QuizQuestionsActivity.kt
@@ -3,6 +3,7 @@ package com.example.quizzz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,22 @@ class QuizQuestionsActivity : AppCompatActivity() {
 
         val questionList=Constants.getQuestions()
         Log.i("Question size","${questionList.size}")
+
+        val currentPos=1
+        val question:Question?=questionList[currentPos-1]
+
+        progressBar.progress=currentPos
+        ProgressText.text= "$currentPos"+ "/" + progressBar.max
+        tvquestion.text=question!!.question
+        flagImage.setImageResource(question.image)
+        OptionOne.text=question.optionOne
+        OptionTwo.text=question.optionTwo
+        OptionThree.text=question.optionThree
+        OptionFour.text=question.optionFour
+
+
+
+
+
     }
 }
